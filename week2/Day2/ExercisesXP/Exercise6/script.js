19b03e3f4c90fe5cdc4b2deede64f6dbc207416a
KarenYFC <148311700+KarenYFC@users.noreply.github.com>
2024-03-10 19:49:08 +0400

    b

---
+++ week2/Day2/ExercisesXP/Exercise6/script.js
@@ -0,0 +1,47 @@
+5 + "34"
+// Prediction: 534
+// Actual: 534
+
+5 - "4"
+// Prediction: 1
+// Actual: 1
+
+10 % 5
+// Prediction: 0
+// Actual: 0
+
+5 % 10
+// Prediction: 5
+// Actual: 5
+
+" " + " "
+// Prediction: 
+// Actual: '  '
+
+" " + 0
+// Prediction: 
+// Actual: ' 0'
+
+true + true
+// Prediction: truetrue
+// Actual: 2
+
+true + false
+// Prediction: truefalse
+// Actual: 1
+
+false + true
+// Prediction: falsetrue
+// Actual: 1
+
+false - true
+// Prediction: NaN
+// Actual: -1
+
+!true
+// Prediction: false
+// Actual: false
+
+3 - 4
+// Prediction: -1
+// Actual: -1
