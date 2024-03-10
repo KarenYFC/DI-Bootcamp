c914c2fdf549ce5d4bbf4a0d6d87e7867f66c571
KarenYFC <148311700+KarenYFC@users.noreply.github.com>
2024-03-10 19:14:42 +0400

    Update script.js

--- week2/Day2/ExercisesXP/Exercise1/script.js
+++ week2/Day2/ExercisesXP/Exercise1/script.js
@@ -1,4 +1,8 @@
+//Store your favorite food into a variable.
 var favoriteFood = ["pizza"];
+
+//Store your favorite meal of the day into a variable (ie. breakfast, lunch or dinner)
 var favoriteMeal = ["dinner"];
 
+//Console.log I eat <favorite food> at every <favorite meal>
 console.log("I eat " + favoriteFood + " at every " + favoriteMeal);
