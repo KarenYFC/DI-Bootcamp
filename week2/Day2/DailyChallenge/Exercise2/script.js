c7756276d5e9e7f00afdddbb2506e9a0eda8bba7
KarenYFC <148311700+KarenYFC@users.noreply.github.com>
2024-03-12 00:11:17 +0400

    o

---
+++ week2/Day2/DailyChallenge/Exercise2/script.js
@@ -0,0 +1,34 @@
+//Using this array :
+const moreFruits = [
+"Banana", 
+[
+    "Apples", ["Oranges"], "Blueberries"
+]
+];
+
+//Access and then console.log “Oranges”.
+console.log(moreFruits[1][1]);
+
+
+
+/*var courseNestedArray = [ 
+["EDUCBA","Online IT Platform",["Hi","Happy"]],
+["Courses", "Java"], 
+["Fee", "5000"], 
+["Hot Course", "Java"] 
+];
+
+console.log(courseNestedArray[0][0]);
+document.write("<h1>Nested Array with Index Accessing Values</h1>");
+//line3
+document.write("0, 0 index array value=>"+courseNestedArray[0][0]+"<br>");
+document.write("0, 1 index array value=>"+courseNestedArray[0][1]+"<br>");
+document.write("1, 0 index array value=>"+courseNestedArray[1][0]+"<br>");
+document.write("1, 1 index array value=>"+courseNestedArray[1][1]+"<br>");
+document.write("2, 0 index array value=>"+courseNestedArray[2][0]+"<br>");
+document.write("2, 1 index array value=>"+courseNestedArray[2][1]+"<br>");
+document.write("3, 0 index array value=>"+courseNestedArray[3][0]+"<br>");
+document.write("3, 1 index array value=>"+courseNestedArray[3][1]+"<br>");
+document.write("0, 2, 0 index array value=>"+courseNestedArray[0][2][0]+"<br>");
+document.write("0, 2, 1 index array value=>"+courseNestedArray[0][2][1]);
+*/
