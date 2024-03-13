a931e82fa07dc5adbf3484f9f00774376b82311a
KarenYFC <148311700+KarenYFC@users.noreply.github.com>
2024-03-13 22:17:11 +0400

    Add files via upload

---
+++ week2/Day4/ExercisesXP/Exercise1/script.js
@@ -0,0 +1,17 @@
+//Create 2 variables, x and y. Each of them should have a different numeric value.
+let x = 2;
+let y = 2;
+
+//Write an if/else statement that checks which number is bigger.
+Example :
+if (x > y) {
+    console.log(`x is the biggest number!`);
+}else if (x == y){
+    console.log(`x is equal to y!`);
+}else{
+    console.log(`y is the biggest number!`);
+}
+
+
+//You should display:
+//x is the biggest number
