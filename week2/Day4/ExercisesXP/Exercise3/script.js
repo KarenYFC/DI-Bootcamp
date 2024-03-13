3a5462d0885b3a7f7f26547115331e32286b5604
KarenYFC <148311700+KarenYFC@users.noreply.github.com>
2024-03-13 22:18:13 +0400

    Add files via upload

---
+++ week2/Day4/ExercisesXP/Exercise3/script.js
@@ -0,0 +1,12 @@
+//Prompt the user for a number and save it to a variable.
+let x = prompt("Give a number for x:", );
+
+//Check whether the variable is even or odd.
+//If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+//If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+if (x%2 === 0){
+    alert("x is an even number")
+}else{
+    alert("x is an odd number")
+}
+
